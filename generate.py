16519002ed0e7e00b5e70944390d3155e63600f8
Alex Chamberlain <alex@alexchamberlain.co.uk>
2019-05-11 15:41:33 +0100

    Consolidate HTML header/footer into base template.

--- generate.py
+++ generate.py
@@ -2,7 +2,16 @@ import json
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+# BASE = "https://pylondinium.org/"
+BASE = "http://localhost:8000/"
+PAGES = ["index.html", "keynote.html", "venue.html", "coc.html"]
+
 env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoescape(["html", "xml"]))
+
+for page in PAGES:
+    template = env.get_template(page)
+    with open(page, "w") as fo:
+        fo.write(template.render(base=BASE))
 
 template = env.get_template("schedule.html")
 
@@ -10,10 +19,10 @@ with open("talks.json") as fo:
     talks = json.load(fo)
 
 with open("schedule.html", "w") as fo:
-    fo.write(template.render(talks=talks))
+    fo.write(template.render(base=BASE, talks=talks))
 
 template = env.get_template("talk.html")
 
 for i, t in enumerate(talks):
     with open(f"talks/talk-{i}.html", "w") as fo:
-        fo.write(template.render(talk=t))
+        fo.write(template.render(base=BASE, talk=t))
