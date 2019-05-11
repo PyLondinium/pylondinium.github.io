10841c606e94bd2eed0c11da4cbaa7eeca538f31
Alex Chamberlain <alex@alexchamberlain.co.uk>
2019-05-11 15:44:44 +0100

    Fixup base url.

--- generate.py
+++ generate.py
@@ -1,9 +1,13 @@
+import argparse
 import json
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-# BASE = "https://pylondinium.org/"
-BASE = "http://localhost:8000/"
+parser = argparse.ArgumentParser(description="Generate pylondinium.org website.")
+parser.add_argument("--base", default="https://pylondinium.org/", help="Base URL")
+
+args = parser.parse_args()
+
 PAGES = ["index.html", "keynote.html", "venue.html", "coc.html"]
 
 env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoescape(["html", "xml"]))
@@ -11,7 +15,7 @@ env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoes
 for page in PAGES:
     template = env.get_template(page)
     with open(page, "w") as fo:
-        fo.write(template.render(base=BASE))
+        fo.write(template.render(base=args.base))
 
 template = env.get_template("schedule.html")
 
@@ -19,10 +23,10 @@ with open("talks.json") as fo:
     talks = json.load(fo)
 
 with open("schedule.html", "w") as fo:
-    fo.write(template.render(base=BASE, talks=talks))
+    fo.write(template.render(base=args.base, talks=talks))
 
 template = env.get_template("talk.html")
 
 for i, t in enumerate(talks):
     with open(f"talks/talk-{i}.html", "w") as fo:
-        fo.write(template.render(base=BASE, talk=t))
+        fo.write(template.render(base=args.base, talk=t))
